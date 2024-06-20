9d04983350f91b0ce4b199c9c585c6379bae4518
kudes <kasenov2002@mail.ru>
2024-06-21 03:11:11 +0500

    add MainLayout

--- src/App.js
+++ src/App.js
@@ -3,29 +3,22 @@ import { Routes, Route } from "react-router-dom";
 
 import "./scss/app.scss";
 
-import Header from "./components/Header";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
+import FullPizza from "./pages/FullPizza";
 import NotFound from "./pages/NotFound";
-
-export const SearchContext = React.createContext();
+import MainLayout from "./layouts/MainLayout";
 
 function App() {
-  const [searchValue, setSearchValue] = React.useState("");
-
   return (
-    <div className="wrapper">
-      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
-        <Header />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </SearchContext.Provider>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="" element={<Home />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="pizza/:id" element={<FullPizza />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
